7feeab1a708f36829f602597a12b0ea4adeb7c74
ricky2803 <ricky.rap28@gmail.com>
2025-06-28 07:27:20 +0700

    refactor

---
+++ src/JurnalUmumServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Altamasoft\JurnalUmum;
+
+use Illuminate\Support\ServiceProvider;
+
+class JurnalUmumServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Daftarkan binding, config, dsb
+    }
+
+    public function boot()
+    {
+        // Load route, migration, dsb
+    }
+}
