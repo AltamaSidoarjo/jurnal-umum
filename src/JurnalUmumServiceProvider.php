d7bed95703cbd233f20396190dce7ad1af9de76a
ricky2803 <ricky.rap28@gmail.com>
2025-06-28 08:27:54 +0700

    fixed view

--- src/JurnalUmumServiceProvider.php
+++ src/JurnalUmumServiceProvider.php
@@ -16,5 +16,6 @@ class JurnalUmumServiceProvider extends ServiceProvider
         // Load route, migration, dsb
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'jurnalumum');
     }
 }
