bf25276bec1b28572f522479cb3a277e7824553b
ricky2803 <ricky.rap28@gmail.com>
2025-06-28 07:29:19 +0700

    fixed: mendaftarkan routes dan migration

--- src/JurnalUmumServiceProvider.php
+++ src/JurnalUmumServiceProvider.php
@@ -14,5 +14,7 @@ class JurnalUmumServiceProvider extends ServiceProvider
     public function boot()
     {
         // Load route, migration, dsb
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
     }
 }
